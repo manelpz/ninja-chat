b0da1fd898ac370bfcf604c3b36715ea1088d4fd
Emmanuel López Guerrero <manelpz@icloud.com>
2020-12-31 22:38:17 -0600

    adding firebase

---
+++ src/firebase/init.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
+
+//initialize Firebase
+var config = {
+    apiKey: "AIzaSyBsjG8mEULDD26xyQaZx5kvf_NylgQALJg",
+    authDomain: "udemy-ninja-smoothies-4afe2.firebaseapp.com",
+    databaseURL: "https://udemy-ninja-smoothies-4afe2.firebaseio.com",
+    projectId: "udemy-ninja-smoothies-4afe2",
+    storageBucket: "udemy-ninja-smoothies-4afe2.appspot.com",
+    messagingSenderId: "261952979249",
+    appId: "1:261952979249:web:beacbf6026a9b2e7dc3010",
+    measurementId: "G-HW5JTT147H"
+  };
+  //config of firebase
+  const firebaseApp = firebase.initializeApp(config);
+  firebaseApp.firestore().settings({timestampsInSnapshots: true})
+  //export firestone database
+  export default firebaseApp.firestore()
