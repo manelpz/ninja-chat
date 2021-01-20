843cbae2f34ae0701a24c8db66a2cb2b18fdee70
Emmanuel López Guerrero <manelpz@icloud.com>
2021-01-19 23:31:48 -0600

    adding v-model

--- src/components/NewMessage.vue
+++ src/components/NewMessage.vue
@@ -3,8 +3,19 @@
 <template>
 <div class="new-message">
     <form @submit.prevent="addMessage">
-     <label for="new-message">New message</label>
+     <label for="new-message">New message (enter to add)</label>
+     <input type="text" name="new-message" v-model="">
     </form>
 
 </div>
 </template>
+
+<script>
+    export default{
+        data(){
+            return {
+                NewMessage:null
+            }
+        }
+    }
+</script>
